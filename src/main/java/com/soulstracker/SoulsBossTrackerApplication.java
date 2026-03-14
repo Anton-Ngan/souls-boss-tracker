201e755f4c4afa12347a2a8ed710520717b6064b
Anton Ngan <anton.an.ngan@gmail.com>
2026-03-15 03:00:00 +1100

    chore: initial setup

---
+++ src/main/java/com/soulstracker/SoulsBossTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.soulstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulsBossTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulsBossTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soulstracker/SoulsBossTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulsBossTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
